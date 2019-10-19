14901b6a61ce131fdc37a8af8c14c204de10b9b1
Benamira Adrien <adrien.benamira@supelec.fr>
2019-10-19 11:47:11 +0200

    new_generation_dataset

--- generate_poster.py
+++ generate_poster.py
@@ -3,17 +3,36 @@ import pandas as pd
 import requests
 import time
 config = Config()
+import numpy as np
 
+np.random.seed(config.general.seed)
 
+compteur = 0
 data_csv = pd.read_csv(config.path.data_csv, encoding='latin-1').dropna()
-for i in range(len(data_csv.Poster)):
+n = len(data_csv.Poster)
+liste_index = np.random.permutation(n)
+for i in liste_index:
     try:
         url = data_csv.Poster[i]
-        name_file = data_csv.Title[i][:-7].replace(" ","_")
-        date = data_csv.Title[i][-5:-1]
-        filename = config.path.data_posters + "/" + str(i) + ";" + str(date) + ";" + name_file +".png"
         r = requests.get(url, allow_redirects=True)
-        open(filename, 'wb').write(r.content)
-        time.sleep(0.5)
+        if r.status_code != 404:
+            compteur += 1
+            name_file = data_csv.Title[i][:-7].replace(" ","_")
+            date = data_csv.Title[i][-5:-1]
+            imdbid = data_csv["imdbId"][i]
+            note = 10*data_csv["IMDB Score"][i]
+            genre = data_csv["Genre"][i]
+            name_new_file = str(compteur) + ";" + str(date) + ";" + name_file + ";" + str(imdbid) + ";" + str(note) + ";" + str(genre) +".png"
+            reste = compteur % 100
+            if reste <=64:
+                filename = config.path.data_path_train + "/" + name_new_file
+            elif 64< reste <=80:
+                filename = config.path.data_path_val + "/" + name_new_file
+            else:
+                filename = config.path.data_path_test + "/" +name_new_file
+            open(filename, 'wb').write(r.content)
+        time.sleep(0.05)
     except:
         pass
+    if compteur % 100 == 99:
+        print(i/n)
